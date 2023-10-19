d5b0e8c70e7b678b2884ea1a356191e7c489066c
geiger-eclare <geiger.eclare@gmail.com>
2023-10-19 12:19:09 -0400

    test commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-console.log("Your index.js file is loaded correctly!");
-$("#btnPrimary").on("click", function() {
-    console.log("you clicked me!!");
-})
+$(document).ready(function() {
+    // Select all buttons with class "btnPrimary1"
+    $(".btnPrimary1").hover(function() {
+        // When hovered over, apply CSS transformation to grow card images
+        $(".cardImage1").css("transform", "scale(1.2)");
+    }, function() {
+        // When the hover event ends, remove the transformation
+        $(".cardImage1").css("transform", "scale(1)");
+    });
+});
